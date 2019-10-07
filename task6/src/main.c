6c53a126ea7d529759db7a466912cbb6c6c3e908
U-PHYSICSR305\Advanced Student <Advanced Student@PhysicsR305.kwantlen.ca>
2019-10-07 11:35:12 -0700

    Ociliscope control

---
+++ task6/src/main.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>	
+#include <visa.h>
+
+void main(int argc, char** argv)
+{
+	unsigned char resultBuffer[256];
+	ViStatus status = VI_SUCCESS;
+	ViFindList resourceList;
+	ViUInt32 num_inst;
+	ViUInt32 resultCount;
+
+	ViSession defaultRM, scopeHandle;
+	ViChar description[VI_FIND_BUFLEN];
+	char dataBuffer[2500];
+
+	int y;
+
+	int lsb;
+	int msb;
+
+	status = viOpenDefaultRM(&defaultRM);
+
+	if(status == VI_SUCCESS)
+	{
+		status = viFindRsrc(defaultRM,"USB[0-9]::?*INSTR",	&resourceList,&num_inst,description);
+		if(status == VI_SUCCESS)
+		{
+			status = viOpen(defaultRM,description,	VI_NULL,VI_NULL,&scopeHandle);
+
+			if(status == VI_SUCCESS)
+			{
+				printf("\nOpened %s\n",description);
+
+				/*viWrite(scopeHandle,"CH1:VOLts 1",16,&resultCount);
+				viRead(scopeHandle,resultBuffer,256,&resultCount);*/
+
+				viWrite(scopeHandle,"*IDN?\n",6,&resultCount);      
+				viRead(scopeHandle,resultBuffer,256,&resultCount);
+
+				printf("\nResult count = %d",resultCount);
+				printf("\nResult buffer = %s\n",resultBuffer );
+
+				viWrite(scopeHandle,"CURVe?\n",7,&resultCount);
+				viRead(scopeHandle,dataBuffer,2500,&resultCount);
+
+				for(int i = 0; i<250; i++)
+				{
+					y = dataBuffer[i];
+					printf("\nRaw = %x,  Read = %d",y,y);
+				}
+				
+
+			}
+			else
+			{
+				printf("\nFailed to open %s",description);
+			}
+		}
+		else
+		{
+			printf("\nCouldn't find any instruments");
+		}
+	}
+	else
+	{
+		printf("\nFailed to open defaultRM");
+	}
+}
