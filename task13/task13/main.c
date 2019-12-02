eba4c84cd5f1db1fba674bce081c5b9d0c498cd7
U-PHYSICSR305\Advanced Student <Advanced Student@PhysicsR305.kwantlen.ca>
2019-12-02 09:52:04 -0800

    finish

---
+++ task13/task13/main.c
@@ -0,0 +1,18 @@
+/*
+ * task13.c
+ *
+ * Created: 28/11/2019 12:10:37 PM
+ * Author : Advanced Student
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
